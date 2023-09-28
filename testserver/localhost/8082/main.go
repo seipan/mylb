7934c7604ede53801d5ae2f015ac998486f266e4
yamasaki <yamasakipann0218@gmail.com>
2023-09-29 01:46:34 +0900

    feat : dummy server

--- testserver/localhost/8082/main.go
+++ testserver/localhost/8082/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import "github.com/gin-gonic/gin"
 
+func main() {
+	router := gin.Default()
+	router.GET("/health", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "ok",
+		})
+	})
+	router.GET("/", func(c *gin.Context) {
+		c.Redirect(302, "/health")
+	})
+	router.Run(":8082")
 }
